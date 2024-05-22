6b110600ce4b1939388ad17e98a1e484c3c1eb31
crisvargas25 <cristianarturo022@gmail.com>
2024-05-22 08:42:25 -0600

    updated

--- parcial1_2B_BIS/6_ejercicios_repaso/ejercicioRepaso.py
+++ parcial1_2B_BIS/6_ejercicios_repaso/ejercicioRepaso.py
@@ -9,13 +9,31 @@ se compran de 1 a 5 producto se otorga el 10% de desc.
 Si se compran de 6 a 10 el 15%, y si compran mas de 10 el 20%.
 
 '''
+nombre = input("Ingresa nombre del producto: ")
+codigo = input("Ingresa el código: ")
+cantidad = int(input("Ingresa la cantidad: "))
+punit = float(input("Ingresa el precio unitario: "))
 
-nombre=input("ingresa nombre del producto: ")
-codigo=input("Ingresa el codigo: ")
-cantidad=input("Ingresa la cantidad: ")
-punit=input("ingresa el precio unitario: ")
+if cantidad <= 5:
+    descuento = 0.10  # 10% de descuento
+elif cantidad <= 10:
+    descuento = 0.15  # 15% de descuento
+else:
+    descuento = 0.20  # 20% de descuento
 
-if cantidad<=5:
-    descuento=.9
-elif cantidad<=10:
-    descuento=.85
+total_bruto = cantidad * punit
+
+total_descuento = total_bruto * (1 - descuento)
+
+iva = 0.16
+total_final = total_descuento * (1 + iva)
+
+print("--------------------------")
+print("Nombre del producto:", nombre)
+print("Código del producto:", codigo)
+print("Cantidad:", cantidad)
+print("Precio unitario: ${:.2f}".format(punit))
+print("Descuento aplicado: {:.0f}%".format(descuento * 100))
+print("Total con descuento: ${:.2f}".format(total_descuento))
+print("IVA aplicado: {:.0f}%".format(iva * 100))
+print("Total a pagar (incluyendo IVA): ${:.2f}".format(total_final))
